39eb18d19eefd5b0910378cac10c0ae39d41ffa3
KrishnaManmayi <manmayi.p9@gmail.com>
2022-11-03 23:10:30 +0530

    Adjusted responsiveness and menu toggling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,72 +11,76 @@ import { MenuItem } from "@mui/material";
 import { CryptoContext } from "../store/CryptoContext";
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const { currency, updateCurrency } = useContext(CryptoContext);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const { currency, updateCurrency } = useContext(CryptoContext);
 
-  const menuToggleHandler = () => {
-    setIsMenuOpen((prev) => !prev);
-  };
+    const menuToggleHandler = () => {
+        setIsMenuOpen((prev) => !prev);
+    };
 
-  const changeCurrencyHandler = (event) => {
-    updateCurrency(event.target.value);
-  };
+    const changeCurrencyHandler = (event) => {
+        updateCurrency(event.target.value);
+    };
 
-  return (
-    <div className={styles.navbar + " main-container"}>
-      <header>
-        <div className={styles.iconMain}>
-          <IconContext.Provider value={{ size: "2rem", title: "Decrypto" }}>
-            <GiAbstract021 />
-          </IconContext.Provider>
-          <Link to="/">
-            <p>Decrypto</p>
-          </Link>
+    return (
+        <div className={styles.navbar + " main-container"}>
+            <header>
+                <div className={styles.iconMain}>
+                    <IconContext.Provider
+                        value={{ size: "2rem", title: "Decrypto" }}>
+                        <GiAbstract021 />
+                    </IconContext.Provider>
+                    <Link to="/">
+                        <p>Decrypto</p>
+                    </Link>
+                </div>
+                <div>
+                    <Select
+                        value={currency}
+                        onChange={changeCurrencyHandler}
+                        style={{ width: 100, height: 40, marginLeft: 15 }}>
+                        <MenuItem value={"USD"}>USD</MenuItem>
+                        <MenuItem value={"INR"}>INR</MenuItem>
+                    </Select>
+                </div>
+                <div>
+                    <button
+                        className={styles.responsive + " " + styles.active}
+                        onClick={menuToggleHandler}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                </div>
+            </header>
+            <nav className={isMenuOpen ? styles.active : ""}>
+                <NavLink
+                    onClick={menuToggleHandler}
+                    to="/cryptocurrencies"
+                    className={({ isActive }) =>
+                        isActive ? `${styles.navElementActive}` : ""
+                    }>
+                    Cryptocurrencies
+                </NavLink>
+                <NavLink
+                    onClick={menuToggleHandler}
+                    to="/exchanges"
+                    className={({ isActive }) =>
+                        isActive ? `${styles.navElementActive}` : ""
+                    }>
+                    Exchanges
+                </NavLink>
+                <NavLink
+                    onClick={menuToggleHandler}
+                    to="/news"
+                    className={({ isActive }) =>
+                        isActive ? `${styles.navElementActive}` : ""
+                    }>
+                    News
+                </NavLink>
+            </nav>
         </div>
-        <div>
-          <Select
-            value={currency}
-            onChange={changeCurrencyHandler}
-            style={{ width: 100, height: 40, marginLeft: 15 }}>
-            <MenuItem value={"USD"}>USD</MenuItem>
-            <MenuItem value={"INR"}>INR</MenuItem>
-          </Select>
-        </div>
-        <div>
-          <button
-            className={styles.responsive + " " + styles.active}
-            onClick={menuToggleHandler}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-        </div>
-      </header>
-      <nav className={isMenuOpen ? styles.active : ""}>
-        <NavLink
-          to="/cryptocurrencies"
-          className={({ isActive }) =>
-            isActive ? `${styles.navElementActive}` : ""
-          }>
-          Cryptocurrencies
-        </NavLink>
-        <NavLink
-          to="/exchanges"
-          className={({ isActive }) =>
-            isActive ? `${styles.navElementActive}` : ""
-          }>
-          Exchanges
-        </NavLink>
-        <NavLink
-          to="/news"
-          className={({ isActive }) =>
-            isActive ? `${styles.navElementActive}` : ""
-          }>
-          News
-        </NavLink>
-      </nav>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
--- src/components/TrendingCarousel.js
+++ src/components/TrendingCarousel.js
@@ -4,38 +4,38 @@ import Slider from "react-slick";
 import styles from "./TrendingCarousel.module.css";
 
 const TrendingCarousel = (props) => {
-  var settings = {
-    dots: false,
-    infinite: true,
-    autoplay: true,
-    speed: 1000,
-    autoplaySpeed: 2000,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    rows: 2,
-    responsive: [
-      {
-        breakpoint: 1124,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 400,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+    var settings = {
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        rows: 2,
+        responsive: [
+            {
+                breakpoint: 1124,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
 
-  return (
-    <Slider {...settings} className={styles.slider}>
-      {props.children}
-    </Slider>
-  );
+    return (
+        <Slider {...settings} className={styles.slider}>
+            {props.children}
+        </Slider>
+    );
 };
 
 export default TrendingCarousel;
